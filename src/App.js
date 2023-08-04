79091b97bf6d73ddb4eba0bbadb1067085dcb6d1
reyarqueza <reyarqueza@gmail.com>
2023-08-04 11:42:25 -0700

    Winner (using calculateWinner util from docs)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, {useState} from 'react';
 import Square from './Square';
 import _times from 'lodash.times';
+import calculateWinner from './utils/calculate-winner';
 
 export default function Board() {
   const [squares, setSquares] = useState([...Array(9)].map(item => ''));
   const [turn, toggleTurn] = useState('X');
+  const winner = calculateWinner(squares);
 
   function handleClick(index) {
     let updatedSquares = [...squares];
 
-    if (updatedSquares[index]) {
+    if (updatedSquares[index] || winner) {
       return;
     }
 
@@ -20,6 +22,9 @@ export default function Board() {
 
   return (
     <>
+      {winner &&
+        <h1>{winner} has won!</h1>
+      }
       {squares.map((square, index) => (index % 3 === 0) &&
         <div key={index} className="board-row">
           {_times(3, i =>
